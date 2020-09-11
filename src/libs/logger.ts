e44db66fda1de23e5c68072c8415a3f9a70d3b3b
Aryan Singhal <aryan.singhal@lapr247.successivetech.com>
2020-09-11 11:32:47 +0530

    add winston-mongodb

--- src/libs/logger.ts
+++ src/libs/logger.ts
@@ -1,18 +1,26 @@
 import winston, { createLogger, transports, format } from 'winston';
+import { MongoDB } from 'winston-mongodb';
+import config from '../config/configuration';
+
 const { combine, timestamp, printf, colorize } = format;
 
 const myFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`;
 });
 
-const transport = new transports.Console();
 const logger = createLogger({
   format: combine(
     timestamp(),
     colorize(),
     myFormat,
   ),
-  transports: [new transports.Console()]
+  transports: [
+    new transports.Console(),
+    new MongoDB({
+      level: 'error',
+      db: config.mongoUrl
+    })
+  ]
 });
 
 export default logger;
